6b6bb2f6d080dc9690f5fae88b5ab0a6094ed16e
Silvio Traversaro <silvio@traversaro.it>
2025-07-06 17:19:46 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -15,6 +15,8 @@ if [ "${build_platform}" != "${target_platform}" ]; then
   CMAKE_ARGS="${CMAKE_ARGS} -DPython3_SOABI:STRING=${PYTHON_EXTENSION_SUFFIX}"
 fi
 
+echo "==============> CMAKE_ARGS: ${CMAKE_ARGS}"
+
 # CMAKE_REQUIRE_FIND_PACKAGE_Python3 is used to early fail at CMake configuration if Python3 can't be found,
 # see https://cmake.org/cmake/help/latest/variable/CMAKE_REQUIRE_FIND_PACKAGE_PackageName.html
 cmake -G Ninja -B build \
