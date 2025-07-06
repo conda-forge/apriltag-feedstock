4a71668c3943fcf1add1ea1ec03f2362284a42f2
Silvio Traversaro <silvio@traversaro.it>
2025-07-06 16:02:21 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,13 @@ CMAKE_ARGS="${CMAKE_ARGS} -DPython3_EXECUTABLE:PATH=${PYTHON}"
 CMAKE_ARGS="${CMAKE_ARGS} -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
 CMAKE_ARGS="${CMAKE_ARGS} -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
 
+# if cross-compiling, hardcode the SOABI to help CMake
+# see https://github.com/conda-forge/htcondor-feedstock/pull/213/commits/c740e27124aa91a36e6a10e6f411d345582440ac
+if [ "${build_platform}" != "${target_platform}" ]; then
+  PYTHON_EXTENSION_SUFFIX=$(${PYTHON}${PY_VER}-config --extension-suffix | cut -d. -f2)
+  CMAKE_ARGS="${CMAKE_ARGS} -DPython3_SOABI:STRING=${PYTHON_EXTENSION_SUFFIX}"
+fi
+
 # CMAKE_REQUIRE_FIND_PACKAGE_Python3 is used to early fail at CMake configuration if Python3 can't be found,
 # see https://cmake.org/cmake/help/latest/variable/CMAKE_REQUIRE_FIND_PACKAGE_PackageName.html
 cmake -G Ninja -B build \
