e2c12d024733ffaee72ac2c7cbe73887e824e139
Silvio Traversaro <silvio@traversaro.it>
2025-07-06 17:24:32 +0200

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,8 +11,8 @@ CMAKE_ARGS="${CMAKE_ARGS} -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR
 # if cross-compiling, hardcode the SOABI to help CMake
 # see https://github.com/conda-forge/htcondor-feedstock/pull/213/commits/c740e27124aa91a36e6a10e6f411d345582440ac
 if [ "${build_platform}" != "${target_platform}" ]; then
-  PYTHON_EXTENSION_SUFFIX=$(${PYTHON}${PY_VER}-config --extension-suffix | cut -d. -f2)
-  CMAKE_ARGS="${CMAKE_ARGS} -DPython3_SOABI:STRING=${PYTHON_EXTENSION_SUFFIX}"
+  Python_SOABI="$(python -c 'import sysconfig; print(sysconfig.get_config_var("EXT_SUFFIX"))')"
+  CMAKE_ARGS="${CMAKE_ARGS} -DPython3_SOABI:STRING=${Python_SOABI}"
 fi
 
 echo "==============> CMAKE_ARGS: ${CMAKE_ARGS}"
