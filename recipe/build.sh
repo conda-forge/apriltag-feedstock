2b912e18922f3bfbea6b7d5a5f43462c2c8e11d6
Silvio Traversaro <silvio@traversaro.it>
2025-09-04 13:16:08 +0200

    Update build.sh to include Python and NumPy paths

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -ex
 
+# As documented in https://conda-forge.org/docs/maintainer/knowledge_base/#finding-numpy-in-cross-compiled-python-packages-using-cmake
+Python3_INCLUDE_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("include"))')"
+Python3_NumPy_INCLUDE_DIR="$(python -c 'import numpy;print(numpy.get_include())')"
+CMAKE_ARGS="${CMAKE_ARGS} -DPython3_EXECUTABLE:PATH=${PYTHON}"
+CMAKE_ARGS="${CMAKE_ARGS} -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
+CMAKE_ARGS="${CMAKE_ARGS} -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
+
 cmake -G Ninja -B build \
     ${CMAKE_ARGS} \
     -D BUILD_SHARED_LIBS=ON \
-    -D PYTHON_EXECUTABLE=${PYTHON} \
     -D BUILD_PYTHON_WRAPPER=ON \
     -D BUILD_TESTING=ON
 cmake --build build --target install
